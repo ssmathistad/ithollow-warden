da33251efe71443f1931156c1783a726af5779a6
ssmathistad <ssmathistad@mail.csuchico.edu>
2021-12-21 12:15:11 -0800

    fix: add json_patch for billing label

--- app/warden-mutating.py
+++ app/warden-mutating.py
@@ -16,19 +16,24 @@ def deployment_webhook():
             #Send response back to controller if validations succeeds
             return k8s_response(True, uid, "Billing label exists")
     except:
-        return k8s_response(False, uid, "No labels exist. A Billing label is required")
+        return k8s_response(False, uid, "No labels exist. A Billing label is required. Adding one now.")
 
     #Send response back to controller if failed
-    return k8s_response(False, uid, "Not allowed without a billing label")
+    return k8s_response(False, uid, "Not allowed without a billing label. Adding one now.")
 
 #Function to respond back to the Admission Controller
 def k8s_response(allowed, uid, message):
-    if allowed  == True:
-        json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/billing", "value": "customer1"}])
-        base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
-        return jsonify({"apiVersion": "admission.k8s.io/v1", "kind": "AdmissionReview", "response": {"allowed": allowed, "uid": uid, "status": {"message": message}, "patchType": "JSONPatch", "patch": base64_patch}})
+    json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/billing", "value": "customer1"}])
+    json_pat
+    base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
+    return jsonify({"apiVersion": "admission.k8s.io/v1", "kind": "AdmissionReview", "response": {"allowed": True, "uid": uid, "status": {"message": message}, "patchType": "JSONPatch", "patch": base64_patch}})
+
+    # if allowed  == True:
+    #     json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/billing", "value": "customer1"}])
+    #     base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
+    #     return jsonify({"apiVersion": "admission.k8s.io/v1", "kind": "AdmissionReview", "response": {"allowed": allowed, "uid": uid, "status": {"message": message}, "patchType": "JSONPatch", "patch": base64_patch}})
         
-    return jsonify({"apiVersion": "admission.k8s.io/v1", "kind": "AdmissionReview", "response": {"allowed": allowed, "uid": uid, "status": {"message": message}}})
+    # return jsonify({"apiVersion": "admission.k8s.io/v1", "kind": "AdmissionReview", "response": {"allowed": allowed, "uid": uid, "status": {"message": message}}})
 
 if __name__ == '__main__':
     warden.run(ssl_context=('certs/wardencrt.pem', 'certs/wardenkey.pem'),debug=True, host='0.0.0.0')
